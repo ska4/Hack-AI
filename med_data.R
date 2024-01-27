c0b73312e9bb010da05d4eeb70caf96c0d5c2f0a
ska4 <147550306+ska4@users.noreply.github.com>
2024-01-27 17:14:31 -0600

    Add files via upload

---
+++ med_data.R
@@ -0,0 +1,18 @@
+med_data <- read.csv("meddata.csv")
+
+library(ggplot2)
+library(tidyverse)
+library(DT)
+library(reticulate)
+library(dotenv)
+
+
+cough <- ggplot(med_data, aes(Cough, fill = Disease))+ geom_bar()
+fever <- ggplot(med_data, aes(Fever, fill = Disease))+ geom_bar()
+fatigue <- ggplot(med_data, aes(Fatigue, fill = Disease))+ geom_bar()
+difficult_breathing <- ggplot(med_data, aes(Difficulty.Breathing, fill = Disease))+ geom_bar()
+bp <- ggplot(med_data, aes(Blood.Pressure, fill = Disease))+ geom_bar()
+chol <- ggplot(med_data, aes(Cholesterol.Level, fill = Disease))+ geom_bar()
+
+
+source_python("openai.py")
